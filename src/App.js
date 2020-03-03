a7207bfe4ee9b553aa52cd9e19389fef1f6df998
Augusto Amaral Pereira <guto_wow@hotmail.com>
2020-03-03 21:01:18 +0100

    Prettified files

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import './App.css';
-import jwtDecode from 'jwt-decode';
-import axios from 'axios';
-import AuthRoute from './util/AuthRoute';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import jwtDecode from "jwt-decode";
+import axios from "axios";
+import AuthRoute from "./util/AuthRoute";
 
 //Pages
-import home from './pages/home';
-import signup from './pages/signup';
-import login from './pages/login';
+import home from "./pages/home";
+import signup from "./pages/signup";
+import login from "./pages/login";
 
 let authenticated = false;
 
 const token = localStorage.FBIdToken;
 
-if(token) {
+if (token) {
   const decodedToken = jwtDecode(token);
-  if(decodedToken * 1000 < Date.now()) {
-    window.location.href = '/login';
+  if (decodedToken * 1000 < Date.now()) {
+    window.location.href = "/login";
     authenticated = false;
   } else {
     authenticated = true;
-    axios.defaults.headers.common['Authorization'] = token;
-    console.log(authenticated)
+    axios.defaults.headers.common["Authorization"] = token;
+    console.log(authenticated);
   }
 }
-
 
 function App() {
   return (
@@ -36,8 +35,18 @@ function App() {
         <div className="container">
           <Switch>
             <Route exact path="/" component={home} />
-            <AuthRoute exact path="/signup" component={signup} authenticated={authenticated} />
-            <AuthRoute exact path="/login" component={login} authenticated={authenticated} />
+            <AuthRoute
+              exact
+              path="/signup"
+              component={signup}
+              authenticated={authenticated}
+            />
+            <AuthRoute
+              exact
+              path="/login"
+              component={login}
+              authenticated={authenticated}
+            />
           </Switch>
         </div>
       </Router>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,44 @@
-import React, { Component } from 'react';
-import { Link, Redirect } from 'react-router-dom';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+import React, { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
 //import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-
+import Button from "@material-ui/core/Button";
 
 export function Navbar({ authenticated }) {
-
   const logout = () => {
     localStorage.clear();
     window.location.reload(false);
-  }
+  };
 
-    return (
-      <AppBar position='fixed'>
-        <Toolbar className="menu">
-          {(authenticated) ? (
-           <div>
-              <Button color="inherit" component={Link} to='/'>Home</Button>
-              <Button color="inherit" onClick={logout}>Logout</Button>
-           </div>)
-           : 
-            <div>
-              <Button color="inherit" component={Link} to='/signup'>Signup</Button>
-              <Button color="inherit" component={Link} to='/'>Home</Button>
-              <Button color="inherit" component={Link} to='/login'>Login</Button>
-            </div>
-          }
-        </Toolbar>
-      </AppBar>
-    ) 
+  return (
+    <AppBar position="fixed">
+      <Toolbar className="menu">
+        {authenticated ? (
+          <div>
+            <Button color="inherit" component={Link} to="/">
+              Home
+            </Button>
+            <Button color="inherit" onClick={logout}>
+              Logout
+            </Button>
+          </div>
+        ) : (
+          <div>
+            <Button color="inherit" component={Link} to="/signup">
+              Signup
+            </Button>
+            <Button color="inherit" component={Link} to="/">
+              Home
+            </Button>
+            <Button color="inherit" component={Link} to="/login">
+              Login
+            </Button>
+          </div>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
 }
 
-export default Navbar
-          
+export default Navbar;
