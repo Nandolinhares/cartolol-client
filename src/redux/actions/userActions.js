e16d3605d9ca1d85af455041e7df016edb2242c0
Nando Linhares <fernandolinhares1920@globo.com>
2020-03-06 22:00:00 -0300

    updating

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -16,6 +16,6 @@ export const loginUser = (userData) => (dispatch) => {
             dispatch({
                 type: SET_ERRORS,
                 payload: err.response.data 
-            });
+            });//
         })
 } 
