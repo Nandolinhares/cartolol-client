492ab82534a22c1f53d7e89a2bed5a6cdbd8ea60
Nando Linhares <fernandolinhares1920@globo.com>
2020-03-08 11:58:03 -0300

    applying authenticated state inside navbat component functional

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,26 +4,55 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 //import Typography from '@material-ui/core/Typography';
 import Button from "@material-ui/core/Button";
+import PropTypes from 'prop-types';
+//Redux Stuff
+import { connect } from 'react-redux';
+import { logoutUser } from '../redux/actions/userActions';
+import store from '../redux/store';
 
-export function Navbar() {
- 
+export function Navbar({ user: { authenticated } }) {
+
+  function handleLogout(){
+    window.location.href = "/login";
+    store.dispatch(logoutUser());
+  }
+
   return (
     <AppBar position="fixed">
       <Toolbar className="menu">
-          <div>   
-            <Button color="inherit" component={Link} to="/signup">
-              Signup
-            </Button>
+        {authenticated ? (
+          <div>
             <Button color="inherit" component={Link} to="/">
               Home
             </Button>
-            <Button color="inherit" component={Link} to="/login">
-              Login
-            </Button>
-          </div> 
+            <Button color="inherit" onClick={handleLogout}>Logout</Button>
+          </div>
+        ) : (<div>
+              <Button color="inherit" component={Link} to="/signup">
+                Signup
+              </Button>
+              <Button color="inherit" component={Link} to="/">
+                Home
+              </Button>
+              <Button color="inherit" component={Link} to="/login">
+                Login
+              </Button>
+            </div>)}
       </Toolbar>
     </AppBar>
   );
 }
 
-export default Navbar;
+Navbar.propTypes = {
+  user: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+  user: state.user
+});
+
+const mapActionsToProps = {
+  logoutUser
+}
+
+export default connect(mapStateToProps, mapActionsToProps)(Navbar);
